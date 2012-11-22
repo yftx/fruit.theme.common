a1e087ec6d2d2a4b878c9df4a210c86d01205ed4
aka2006 <153568795@qq.com>
2012-11-22 15:28:13 +0800

    initial version

---
+++ src/com/fruit/launcher/theme/common/ThemeActivity.java
@@ -0,0 +1,13 @@
+package com.fruit.launcher.theme.common;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThemeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
